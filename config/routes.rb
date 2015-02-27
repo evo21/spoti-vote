d0d8d6b39c506f1cddda8f224065ab7f158d16b9
Dylan Dechant <dylan.dechant@gmail.com>
2015-02-26 22:42:17 -0500

    update routes?

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :suggestions
-  get '/', to: 'users#index', as: 'dashboard'
+  get '/', to: 'suggestions#index', as: 'dashboard'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
